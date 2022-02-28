fb049e4ae4a536921a032f9f3832c36ef15fafee
jeremyzhli <jeremyzh.li@gmail.com>
2022-02-28 15:41:52 +1100

    Add hello world program

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Prints a generic line of text that programmers are in love with
+
+print("Hello world.")
+
+if input("Enter password: ") == "cringe":
+    print("cringe is correct, good sir.")
+else:
+    print("you are cringe, good sir.")
